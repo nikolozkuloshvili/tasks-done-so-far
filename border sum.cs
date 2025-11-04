9c1d18dac71eb81f476e9a36b91d4a6bb98f3304
NIkoloz k <kuloshvilinika@yahoo.com>
2025-11-04 18:42:52 +0400

    Add files via upload

---
+++ border sum.cs
@@ -0,0 +1,39 @@
+﻿namespace codename01112025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Random.Shared.Next(2, 13);
+            int[,] x = new int[size, size];
+            int mid = size / 2;
+            int borderSum = 0;
+
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    x[i, j] = Random.Shared.Next(10, 11);
+
+                    if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        borderSum += x[i, j];
+                    }
+                    else
+                    {
+                        Console.ResetColor();
+                    }
+
+                    Console.Write($"{x[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.WriteLine($"Border sum = {borderSum}");
+        }
+    }
+}
