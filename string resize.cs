37520e7a4668255ff991de0f035d775b0aeff0c2
NIkoloz k <kuloshvilinika@yahoo.com>
2025-11-26 11:00:01 +0400

    Rename variables for clarity in string resize logic
    removed 1 for (no name found)

--- string resize.cs
+++ string resize.cs
@@ -6,33 +6,32 @@ namespace G10_20251121
         {
             Console.Write("How many Names should i write? = ");
 
-            int newSize = Convert.ToInt32(Console.ReadLine());
+            int newLength = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Here are the {newSize} names as you requested: ");
+            Console.WriteLine($"Here are the {newLength} names as you requested: ");
 
             string[] array = { "nikoloz", "giorgi", "tamta", "david", "ana", "luka", "saba", "nino", "elene" };
 
-            Resize(ref array, newSize);
+            Resize(ref array, newLength);
 
             for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine();
-                Console.WriteLine(array[i]);
+                Console.WriteLine(array[i] ?? "No name found");
             }
         }
-        static void Resize(ref string[] array, int newSize)
+        static void Resize(ref string[] array, int newLength)
         {
-            string[] newArray = new string[newSize];
-
-            for (int i = 0; i < array.Length && i < newSize; i++)
+            string[] newArray = new string[newLength];
+            int Length = array.Length < newLength ? array.Length : newLength;
+            if (array.Length == newLength)
+            {
+                return;
+            }
+            for (int i = 0; i < array.Length && i < newLength; i++)
             {
                 newArray[i] = array[i];
             }
-            for (int i = array.Length; i < newSize; i++)
-            {
-                newArray[i] = "No name found";
-            }
-
             array = newArray;
         }
     }
