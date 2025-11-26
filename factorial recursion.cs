24e97f6d91bcf8ac5233ea948a775ab264077c3f
NIkoloz k <kuloshvilinika@yahoo.com>
2025-11-26 11:27:10 +0400

    Update factorial recursion.cs

--- factorial recursion.cs
+++ factorial recursion.cs
@@ -7,20 +7,30 @@ namespace G10_20251121
             Console.Write("Enter a number to calculate its factorial: ");
 
             int factorialNum = Convert.ToInt32(Console.ReadLine());
+
             if (factorialNum < 0)
             {
                 Console.WriteLine("Factorial is not defined for negative numbers.");
                 return;
             }
+
+            if (factorialNum == 0)
+            {
+                Console.WriteLine($"The factorial of {factorialNum} is: 1 "); ;
+                return;
+            }
+
             int result = writeFactorialNumbers(factorialNum);
+
             Console.WriteLine($"The factorial of {factorialNum} is: {result} ");;
 
         }
         static int writeFactorialNumbers(int factorialNum)
         {
             if (factorialNum == 1)
+            {           
                 return 1;
-
+            }
             return factorialNum * writeFactorialNumbers(factorialNum - 1);
         }
     }
